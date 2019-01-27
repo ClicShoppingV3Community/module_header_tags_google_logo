df212d64f48d3f83efee24f3f8f2f1d7902b9cdc
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-27 14:05:02 -0500

    new module

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # module_header_tags_google_logo
+
 This module allow you to display your logo for google  via banner manager
+
+This module contains
+
+- The language files in English and French
+
+Via the installation system administration ClicShopping
+
+Technical Prerequisites: None
+
+Modules:
+
+- Compatibility: >= version 3.0
+
+Recommendation and documentation specific use :
+
+
+Implementation:
+
+- Install the module
+- Activate the module
+
+All informations about the ClicShopping
+
+ Download ClicShopping : https://github.com/ClicShopping/ClicShopping_V3/archive/master.zip
+
+ Community : https://www.clicshopping.org
+
+ Software : https://github.com/ClicShopping
+
+ Official add on : https://github.com/ClicShoppingOfficialModulesV3
+
+ Community add on : https://github.com/ClicShoppingV3Community
+
+ trademark License info : https://www.clicshopping.org/forum/trademark/ 
---
+++ includes/modules/header_tags/ht_google_logo.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+  use ClicShopping\OM\Registry;
+  use ClicShopping\OM\HTTP;
+  use ClicShopping\OM\CLICSHOPPING;
+
+  class ht_google_logo {
+    public $code;
+    public $group;
+    public $title;
+    public $description;
+    public $sort_order;
+    public $enabled = false;
+
+    public function __construct() {
+      $this->code = get_class($this);
+      $this->group = basename(__DIR__);
+
+      $this->title = CLICSHOPPING::getDef('module_header_tags_google_logo_meta_title');
+      $this->description = CLICSHOPPING::getDef('module_header_tags_google_logo_meta_description');
+
+      if ( defined('MODULE_HEADER_TAGS_GOOGLE_LOGO_META_STATUS') ) {
+        $this->sort_order = MODULE_HEADER_TAGS_GOOGLE_LOGO_META_SORT_ORDER;
+        $this->enabled = (MODULE_HEADER_TAGS_GOOGLE_LOGO_META_STATUS == 'True');
+      }
+    }
+
+    public function execute() {
+
+      $CLICSHOPPING_Template = Registry::get('Template');
+      $CLICSHOPPING_Customer = Registry::get('Customer');
+      $CLICSHOPPING_Db = Registry::get('Db');
+
+      if (!$CLICSHOPPING_Customer->isLoggedOn()) {
+
+        if (MODULE_HEADER_TAGS_GOOGLE_LOGO_META_BANNER_GROUP != '') {
+
+           $banners_group = MODULE_HEADER_TAGS_GOOGLE_LOGO_META_BANNER_GROUP;
+
+          $Qbanner = $CLICSHOPPING_Db->prepare('select banners_image,
+                                                 banners_group
+                                          from :table_banners
+                                          where banners_group = :banners_group
+                                          limit 1
+                                        ');
+          $Qbanner->bindValue(':banners_group', $banners_group);
+          $Qbanner->execute();
+
+          $banner = $Qbanner->fetch();
+
+          $footer_tag = '<!--  google logo start -->' . "\n";
+          $footer_tag .= '<script type="application/ld+json"> ';
+          $footer_tag .= '{ ';
+          $footer_tag .= '"@context": "https://schema.org", ';
+          $footer_tag .= '"@type": "Organization", ';
+          $footer_tag .= '"url": "' . HTTP::typeUrlDomain() . '", ';
+          $footer_tag .= '"logo": "' . HTTP::getShopUrlDomain() . $CLICSHOPPING_Template->getDirectoryTemplateImages().  $banner['banners_image'] . '" ';
+          $footer_tag .= '} ';
+          $footer_tag .= '</script>' . "\n";
+
+          $footer_tag .= '<!-- google logo end -->' . "\n";
+
+          $CLICSHOPPING_Template->addBlock($footer_tag, 'footer_scripts');
+        }
+      }
+
+    }
+
+    public function isEnabled() {
+      return $this->enabled;
+    }
+
+    public function check() {
+      return defined('MODULE_HEADER_TAGS_GOOGLE_LOGO_META_STATUS');
+    }
+
+    public function install() {
+      $CLICSHOPPING_Db = Registry::get('Db');
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Souhaitez vous activer ce module ?',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GOOGLE_LOGO_META_STATUS',
+          'configuration_value' => 'True',
+          'configuration_description' => 'Souhaitez vous activer ce module ?',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Veuillez indiquer le groupe de la banni&egrave;re contenant le logo',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GOOGLE_LOGO_META_BANNER_GROUP',
+          'configuration_value' => '',
+          'configuration_description' => 'Veuillez vous référer a&grave; au menu banni&egrave;re pour connaitre le groupe du logo',
+          'configuration_group_id' => '6',
+          'sort_order' => '2',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Ordre de tri d\'affichage',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GOOGLE_LOGO_META_SORT_ORDER',
+          'configuration_value' => '100',
+          'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
+          'configuration_group_id' => '6',
+          'sort_order' => '80',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
+      );
+
+       return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
+                                               ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+                            );
+    }
+
+    public function remove() {
+      return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
+    }
+
+    public function keys() {
+      return array('MODULE_HEADER_TAGS_GOOGLE_LOGO_META_STATUS',
+                   'MODULE_HEADER_TAGS_GOOGLE_LOGO_META_BANNER_GROUP',
+                   'MODULE_HEADER_TAGS_GOOGLE_LOGO_META_SORT_ORDER'
+                  );
+    }
+  }
