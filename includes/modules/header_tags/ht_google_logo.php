99d197c47701b2fc89ae44a9742d323dd29c2b99
ClicShopping <Yae8mr2o*>
2021-03-08 16:33:54 +0100

    update for 3.300 version

--- includes/modules/header_tags/ht_google_logo.php
+++ includes/modules/header_tags/ht_google_logo.php
@@ -15,8 +15,8 @@
 
   class ht_google_logo
   {
-    public string $code;
-    public string $group;
+    public $code;
+    public $group;
     public string $title;
     public string $description;
     public ?int $sort_order = 0;
